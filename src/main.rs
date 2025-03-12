b03955ad51167f6a407f9b9eb0f9c1566c5783d9
zhijing <zhijing041215@gmail.com>
2025-03-12 16:16:21 +0800

    start iplookup backend

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(hello))
+    })
+    .bind("127.0.0.1:6666")?
+    .run()
+    .await
+}
